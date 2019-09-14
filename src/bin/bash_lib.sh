bc267f9689a359e67d3c8e48d546eddd8ea68b49
Martin VanWinkle III <mvanwinkle@ias.edu>
2019-09-14 09:19:37 -0400

    error checking.

---
+++ src/bin/bash_lib.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+function debug_message
+{
+	>&2 echo "DEBUG: $@"
+}
+
+function kill_pid_from_file
+{
+	local file_name="$1"
+	
+	if [[ -e "$file_name" ]]
+	then
+	
+		kill $( cat "$file_name" )
+		rm -f "$file_name"
+	fi
+}
+
+function clean_up_and_exit
+{
+	local msg="$1"; shift
+
+	local end_time=$( date +%s )
+	
+	local duration="$(($end_time-$start_time))"
+
+	kill_pid_from_file "$dhclient_pid_file"
+	kill_pid_from_file "$wpa_pid_file"	
+
+	echo "$nagios_status: $nagios_service_name | ${duration}seconds"
+
+	if [[ ! -z "$msg" ]]
+	then
+		echo $msg
+	fi
+	
+	exit $nagios_exit
+}
+
+function ip_br_device_exists
+{
+	# This relies on ip returning non-zero
+	# when querying an interface
+	
+	local device="$1"
+
+	ip -br addr show "$device" > /dev/null
+	local result=$?
+	# debug_message "Result of ip exists: $result"
+	echo "$result"
+}
+
+function ip_br_device_status
+{
+	local device="$1"
+
+	local ip_br_text=$( ip -br addr show "$device" )
+	local device_status=$(echo "$ip_br_text" | awk '{print $2}')
+
+	echo "$device_status"
+}
+
+function check_for_ip_br_ipv4
+{
+	local device="$1"; shift
+	local wanted_ip="$1"
+	
+	local ip_br_text=$( ip -br addr show "$device" )
+	debug_message "ip_br_text: ${ip_br_text}"
+	
+	local ip_from_ip=$(echo "$ip_br_text" | awk '{print $3}')
+	local result=$?
+	
+	if [[ "$ip_from_ip" ==  "$wanted_ip" ]]
+	then
+		debug_message "Wanted IP $wanted_ip found."
+	else
+		debug_message "IP not found."
+	fi
+
+	return "$result"
+}
+
+
+
--- src/bin/ias_check_wpa_supplicant.sh
+++ src/bin/ias_check_wpa_supplicant.sh
@@ -1,45 +1,10 @@
 #!/bin/bash
 
 device="$1"; shift
-config="$1"
+config="$1"; shift;
+wanted_ip="$1"
 
-function debug_message
-{
-	>&2 echo "$@"
-}
-
-function kill_pid_from_file
-{
-	local file_name="$1"
-	
-	if [[ -e "$file_name" ]]
-	then
-	
-		kill $( cat "$file_name" )
-		rm -f "$file_name"
-	fi
-}
-
-function clean_up_and_exit
-{
-	msg="$1"; shift
-
-	end_time=$( date +%s )
-	
-	duration="$(($end_time-$start_time))"
-
-	kill_pid_from_file "$dhclient_pid_file"
-	kill_pid_from_file "$wpa_pid_file"	
-
-	echo "$nagios_status: $nagios_service_name | ${duration}seconds"
-
-	if [[ ! -z "$msg" ]]
-	then
-		echo $msg
-	fi
-	
-	exit $nagios_exit
-}
+. bash_lib.sh
 
 start_time=$( date +%s )
 
@@ -48,7 +13,27 @@ nagios_service_name="ias_check_wpa_supplicant $device $config"
 nagios_status="OK"
 nagios_exit="0"
 
+# Prep section
+# ip_exists=$( ip_br_device_exists "$device")
+# debug_message "IP exists: $ip_exists"
 
+if [[ $( ip_br_device_exists "$device") != "0" ]]
+then
+	nagios_status="UNKNOWN"
+	nagios_exit=3
+	clean_up_and_exit "ip can't query device $device .  Does it exist?"
+fi
+
+if [[ $( ip_br_device_status "$device") != "DOWN" ]]
+then
+	nagios_status="UNKNOWN"
+	nagios_exit=3
+	clean_up_and_exit "$device is not down"
+fi
+echo "HERE"
+exit
+
+# Main section
 wpa_pid_file=$(mktemp /tmp/ias_check_wpa_supplicant-wpa_supplicant_pid.XXXXXX)
 
 debug_message "Running wpa_supplicant."
@@ -82,19 +67,12 @@ for i in {1..30}
 do
 	sleep 1
 	debug_message "Looping."
-	ip_br_text=$( ip -br addr show "$device" )
-	debug_message "ip_br_text: ${ip_br_text}"
-	
-	echo "$ip_br_text" | grep '192\.168'
-	result=$?
-	
-	if [[ "$result" == 0 ]]
+
+	if [[ check_for_ip_br ]]
 	then
-		debug_message "IP found."
 		break
-	else
-		debug_message "IP not found."
 	fi
+	
 done
 
 clean_up_and_exit
